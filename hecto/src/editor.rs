6f3a41735f4b444ac13ee65dbb8484629f2c9ea9
Hamid Jamal <hamidlikestocode101@gmail.com>
2025-07-01 15:54:09 +0100

    Raw text input and output done

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -1,40 +1,161 @@
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
-use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use std::io::Error;
+
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+use crossterm::{event::{
+        read, Event::{self}, KeyCode::{self}, KeyEvent, KeyModifiers
+    }};
+
+mod terminal;
+
+use terminal::{Terminal, Size, Position};
+
 pub struct Editor {
-    should_quit : bool,
+    should_quit: bool,
+    current_pos: Position,
+    terminal_size: Size,
 }
 
 impl Editor {
     pub fn default() -> Self {
-        Editor{ should_quit: false}
-    }
-
-    pub fn run(&mut self){
-        if let Err(err) = self.repl() {
-            panic!("{err:?}");
+        Self { 
+            should_quit: false, 
+            current_pos : Position{x:1, y:0},
+            terminal_size: Terminal::size().unwrap_or(Size { width: 80, height: 24 }),
         }
-        print!("Goodbye ! \r\n");
     }
 
-    pub fn repl(&mut self) -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
+    pub fn run(&mut self) {
+        Terminal::initialize().unwrap();
+        let result = self.repl();
+        Terminal::terminate().unwrap();
+        result.unwrap();
+    }
+
+    pub fn repl(&mut self) -> Result<(), Error> {
         loop {
-            if let Key(KeyEvent{
-                            code, modifiers, kind, state
-                        }) = read()? {
-                            println!("Code: {code:?} Modifiers {modifiers:?} Kind {kind:?} State {state:?} \r");
-                            match code {
-                                Char('q') if modifiers == KeyModifiers::CONTROL => {
-                                    self.should_quit = true;
-                                }
-                                _ => (),
-                            }
-                            if self.should_quit {
-                                break;
-                            }
+            self.refresh_screen()?;
+
+            if self.should_quit {
+                break;
+            }
+
+            let event = read()?;
+            self.evaluate_event(&event);
+        }
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event) {
+        match event {
+            // Handle Key Presses
+            Event::Key(KeyEvent { code, modifiers, ..}) => {
+                match code {
+                    // Quit the application
+                    KeyCode::Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                        self.should_quit = true;
+                    }
+
+                    // Normal Cursor Movement
+                    KeyCode::Up => {
+                        self.current_pos.y = self.current_pos.y.saturating_sub(1);
+                    }
+                    KeyCode::Down => {
+                        let height = self.terminal_size.height.saturating_sub(1);
+                        self.current_pos.y = self.current_pos.y.saturating_add(1).min(height);
+                    }
+                    KeyCode::Left => {
+                        if self.current_pos.x != 1 {
+                            self.current_pos.x = self.current_pos.x.saturating_sub(1);                            
+                        }
+                    }
+                    KeyCode::Right => {
+                        let width = self.terminal_size.width.saturating_sub(1);
+                        if self.current_pos.x != width{
+                            self.current_pos.x = self.current_pos.x.saturating_add(1).min(width);
+                        }
+                    }
+
+                    // Page and Line Navigation
+                    KeyCode::PageUp => {
+                        self.current_pos.y = 0;
+                    }
+                    KeyCode::PageDown => {
+                        self.current_pos.y = self.terminal_size.height.saturating_sub(1);
+                    }
+                    KeyCode::Home => {
+                        self.current_pos.x = 0;
+                    }
+                    KeyCode::End => {
+                        self.current_pos.x = self.terminal_size.width.saturating_sub(1);
+                    }
+
+                    // Ignore all other keys
+                    _ => (),
+                }
+            }
+            // Handle Window Resizing
+            Event::Resize(width, height) => {
+                self.terminal_size.width = (*width).into();
+                self.terminal_size.height = (*height).into();
+            }
+            _ => (),
+        }
+    }
+
+    fn draw_welcome_msg() -> Result<(), Error>{
+        let mut welcome_msg = format!("{NAME} editor -- Version {VERSION}");
+        let width = Terminal::size()?.width;
+        let len = welcome_msg.len();
+        // Allowed as padding doesn't have to be exactly in the middle
+        #[allow(clippy::integer_division)]
+        let padding = (width.saturating_sub(len)) / 2;
+        
+        let space = " ".repeat(padding.saturating_sub(1));
+        welcome_msg = format!("~{space}{welcome_msg}");
+        welcome_msg.truncate(width);
+
+        Terminal::print(&welcome_msg)?;
+    
+        Ok(())
+    }   
+
+    fn draw_empty_rows() -> Result<(), Error>{
+        Terminal::print("~")?;
+        Ok(())
+    }
+
+    fn refresh_screen(&self) -> Result<(), Error> {
+        Terminal::hide_cursor()?;
+        if self.should_quit {
+            Terminal::clear_screen()?;
+            Terminal::move_cursor_to(Position{x: 0, y: 0})?;
+            Terminal::print("Goodbye !\r\n")?;
+        } else {
+            Terminal::move_cursor_to(Position{x: 0, y: 0})?;
+            Self::draw_rows()?;
+        }
+        Terminal::move_cursor_to(self.current_pos)?;
+        Terminal::show_cursor()?;
+        Terminal::execute()?;
+        Ok(())
+    }
+
+    fn draw_rows() -> Result<(), Error> {
+        let Size{height, ..} = Terminal::size()?;
+        for current_row in 0..height {
+            Terminal::clear_line()?;
+            #[allow(clippy::integer_division)]
+            if current_row == height / 3{
+                Self::draw_welcome_msg()?;
+            } else {
+                Self::draw_empty_rows()?;
+            }
+            if current_row.saturating_add(1) < height {
+                Terminal::print("\r\n")?;
             }
         }
-        disable_raw_mode()?;
-        Ok(())        
+        Ok(())
     }
 }
---
+++ hecto/src/editor/terminal.rs
@@ -0,0 +1,98 @@
+use crossterm::cursor::{Hide, MoveTo, Show};
+use crossterm::{queue, Command};
+use crossterm::style::Print;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
+use std::io::{stdout, Error, Write};
+use core::fmt::Display;
+
+#[derive(Copy, Clone)]
+pub struct Size {
+    pub height: usize,
+    pub width: usize,
+}
+
+#[derive(Copy, Clone)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+/// Represents the Terminal.Add commentMore actions
+/// Edge Case for platforms where `usize` < `u16`:
+/// Regardless of the actual size of the Terminal, this representation
+/// only spans over at most `usize::MAX` or `u16::size` rows/columns, whichever is smaller.
+/// Each size returned truncates to min(`usize::MAX`, `u16::MAX`)
+/// And should you attempt to set the cursor out of these bounds, it will also be truncated.
+pub struct Terminal {}
+
+impl Terminal {
+    pub fn terminate() -> Result<(), Error> {
+        Self::execute()?;
+        disable_raw_mode()?;
+        Ok(())
+    }
+
+    pub fn initialize() -> Result<(), Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()?;
+        Self::move_cursor_to(Position { x: 0, y: 0 })?;
+        Self::execute()?;
+        Ok(())
+        
+    }
+
+    pub fn clear_screen() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::All))?;
+        Ok(())
+    }
+
+    pub fn clear_line() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
+        Ok(())
+    }
+
+    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
+        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
+        Self::queue_command(MoveTo(position.x as u16, position.y as u16))?;
+        Ok(())
+    }
+
+    pub fn hide_cursor() -> Result<(), Error> {
+        Self::queue_command(Hide)?;
+        Ok(())
+    }
+
+    pub fn show_cursor() -> Result<(), Error> {
+        Self::queue_command(Show)?;
+        Ok(())
+    }
+
+    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+        Self::queue_command(Print(string))?;
+        Ok(())
+    }
+
+
+
+    /// Returns the current size of this Terminal.
+    /// Edge Case for systems with `usize` < `u16`:
+    /// * A `Size` representing the terminal size. Any coordinate `z` truncated to `usize` if `usize` < `z` < `u16`
+    pub fn size() -> Result<Size, Error> {
+        let (width_u16, height_u16) = size()?;
+        #[allow(clippy::as_conversions)]
+        let height = height_u16 as usize;
+        #[allow(clippy::as_conversions)]
+        let width = width_u16 as usize;
+        Ok(Size { height, width })
+    }
+
+    pub fn execute() -> Result<(), Error> {
+        stdout().flush()?;
+        Ok(())
+    }
+
+    pub fn queue_command<T: Command>(command: T) -> Result<(), Error>{
+        queue!(stdout(), command)?;
+        Ok(())
+    }
+}
